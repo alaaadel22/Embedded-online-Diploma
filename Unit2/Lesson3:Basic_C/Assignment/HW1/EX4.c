2b72cf22673d7136b7e47db8271ea7385d94a5dd
alaaadel22 <138072307+alaaadel22@users.noreply.github.com>
2023-09-13 00:04:33 +0300

    Create EX4.c

---
+++ Unit2/Lesson3:Basic_C/Assignment/HW1/EX4.c
@@ -0,0 +1,23 @@
+/*
+ * Name: Alaa Adel Kamal
+ *
+ * Unit 2 lesson 3 (Basic C & loops)
+ *
+ * EX4: Write C Program to Multiply two Floating Point Numbers
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+	float y,x;
+	printf ("Enter two Numbers: ");
+	fflush (stdout);
+	scanf ("%f",&y);
+	fflush(stdin);
+	scanf ("%f",&x);
+    fflush(stdin);
+	printf ("Sum = %f",x * y);
+	return 0;
+}
+
